1c1779a3250b07d8193a4f8446a23a50c32d7ef1
Mostafa Alaa <mostafa.alaa325@yahoo.com>
2024-11-12 23:23:57 +0200

    Changed the bird

--- lib/bird.dart
+++ lib/bird.dart
@@ -5,10 +5,16 @@ class Bird extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // width: 130,
-        // height: 130,
-        child: Image.asset("lib/images/bird.png", width: 130, height: 130,),
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0), // Adjust based on the hidden part
+      child: ClipRect(
+        child: Align(
+          alignment: Alignment.topCenter,
+          heightFactor: 0.65, // Adjust to focus on the visible bird part
+          child: Image.asset("lib/images/bird.png", width: 130, height: 130,),
+        ),
+      ),
     );
+
   }
 }
